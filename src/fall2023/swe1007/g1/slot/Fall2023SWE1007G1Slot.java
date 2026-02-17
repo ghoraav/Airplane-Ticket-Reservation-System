56fe47f26817b2ba5640beeb2a4ce0b663486154
K C Ghoraav <144238131+ghoraav@users.noreply.github.com>
2026-02-17 10:02:13 +0530

    Add files via upload

---
+++ src/fall2023/swe1007/g1/slot/Fall2023SWE1007G1Slot.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fall2023.swe1007.g1.slot;
+
+/**
+ *
+ * @author admin
+ */
+public class Fall2023SWE1007G1Slot {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/fall2023/swe1007/g1/slot/firstclass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fall2023.swe1007.g1.slot;
+
+/**
+ *
+ * @author admin
+ */
+public class firstclass {
+    
+}
